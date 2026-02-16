64bc7aab11852566d42894fd4738920724679ef5
Aju <ajilaries20@gmail.com>
2026-02-16 14:53:22 +0530

    updated the main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 // import 'day3/practice_ui.dart';
-import '../Day4/animations_dialogs.dart';
+import '../Day4/dialog_practice_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:AnimationsDialogs(), // 👈 directly open your practice screen
+      home:DialogPracticeScreen(), // 👈 directly open your practice screen
     );
   }
 }
