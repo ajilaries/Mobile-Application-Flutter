af60d2bb5d915c76294e8a2a16da1d248abd2aa5
Aju <ajilaries20@gmail.com>
2026-02-17 21:10:31 +0530

    detail screen for practice

---
+++ lib/Day5/detail_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class DetailScreen extends StatelessWidget {
+  const DetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Details")),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text("go back"),
+        ),
+      ),
+    ); // TODO: implement build
+  }
+}
