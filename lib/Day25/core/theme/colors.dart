769cb569349775a40930e253fa666bae4e8a9be0
Aju <ajilaries20@gmail.com>
2026-03-16 19:48:57 +0530

    colors for the application

---
+++ lib/Day25/core/theme/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xff6C63FF);
+  static const background = Color(0xffF5F5F5);
+  static const card = Colors.white;
+}
