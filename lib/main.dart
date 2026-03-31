146ede22e01ebf2b9444aa3e3f6431012880017a
Aju <ajilaries20@gmail.com>
2026-03-31 15:28:45 +0530

    main file for the theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,6 @@
 import 'package:flutter/material.dart';
-// import 'Day19/home_screen.dart';
-// import 'Day19/insta_profile.dart';
-// import 'Day20/main.dart';
-import 'Day23/main.dart';
+import 'Day25/app.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  int index = 0;
-
-  final screens = const [SmartApp()];
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: const Color(0xff0F0F1B),
-      ),
-      home: Scaffold(
-        body: screens[index],
-        bottomNavigationBar: BottomNavigationBar(
-          backgroundColor: const Color(0xff1A1A2E),
-          selectedItemColor: Colors.deepPurpleAccent,
-          unselectedItemColor: Colors.white54,
-          currentIndex: index,
-          onTap: (value) => setState(() => index = value),
-          items: const [
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-            BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
-          ],
-        ),
-      ),
-    );
-  }
 }
