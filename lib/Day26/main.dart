2f7b2de6dfd520cb8e26ffa5c8269514651a1569
Aju <ajilaries20@gmail.com>
2026-03-31 15:25:36 +0530

    state management another example

--- lib/Day26/main.dart
+++ lib/Day26/main.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'screens/todo_screen.dart';
+import 'providers/theme_provider.dart';
+import '../Day26/screens/home_screen.dart';
 
 void main() {
   runApp(ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: TodoScreen());
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isDark = ref.watch(themeProvider);
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: isDark ? ThemeData.dark() : ThemeData.light(),
+      home: HomeScreen(),
+    );
   }
 }
